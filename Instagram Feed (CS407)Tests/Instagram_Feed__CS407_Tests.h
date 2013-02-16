335cadffb11ed98fa2ff5a6de0e3ad086292a1d5
Brian Anderson <brian.anderson1222@gmail.com>
2013-02-16 15:58:07 -0600

    Fixed project

---
+++ Instagram Feed (CS407)Tests/Instagram_Feed__CS407_Tests.h
@@ -0,0 +1,13 @@
+//
+//  Instagram_Feed__CS407_Tests.h
+//  Instagram Feed (CS407)Tests
+//
+//  Created by Brian Anderson on 2/16/13.
+//  Copyright (c) 2013 Brian Anderson. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Instagram_Feed__CS407_Tests : SenTestCase
+
+@end
---
+++ Instagram Feed (CS407)Tests/Instagram_Feed__CS407_Tests.m
@@ -0,0 +1,32 @@
+//
+//  Instagram_Feed__CS407_Tests.m
+//  Instagram Feed (CS407)Tests
+//
+//  Created by Brian Anderson on 2/16/13.
+//  Copyright (c) 2013 Brian Anderson. All rights reserved.
+//
+
+#import "Instagram_Feed__CS407_Tests.h"
+
+@implementation Instagram_Feed__CS407_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Instagram Feed (CS407)Tests");
+}
+
+@end
